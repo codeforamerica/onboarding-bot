01e6aa42225e104b1b811072839389399b972cc1
Edmund Korley <korley.em@gmail.com>
2016-06-14 22:58:26 -0700

    Scaffold bot messaging service

---
+++ bot/index.js
@@ -0,0 +1,22 @@
+var Botkit = require('botkit');
+var config = require('../config.json');
+
+var setup = process.env.DEV ? config.settings.dev : config.settings.prod;
+
+var controller = Botkit.slackbot(setup);
+
+controller.spawn(config.tokens).startRTM();
+
+// Set-up handler for bot's direct messaging response
+controller.on('direct_message', function(bot, message) {
+  bot.reply(message, 'Hey there 😜, it\'s just you and me.');
+});
+
+// Set-up handler for bot's direct mention response.
+// Is available in channels that bot is in.
+controller.on('direct_mention', function(bot, message) {
+  bot.reply(message, 'Hey there 😜');
+});
+
+// Export for testing
+module.exports = controller;
