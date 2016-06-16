750a6033d96280705a277298653fcafe67a32bc3
Edmund Korley <korley.em@gmail.com>
2016-06-16 09:54:03 -0700

    Declarative JSON for feeding training text into bot logic

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,7 @@
+var natural = require('natural');
 var Botkit = require('botkit');
 var config = require('../config.json');
+var training = require('./training.json');
 
 var setup = process.env.DEV ? config.settings.dev : config.settings.prod;
 
@@ -7,15 +9,32 @@ var controller = Botkit.slackbot(setup);
 
 controller.spawn(config.authentication).startRTM();
 
+var classifier = new natural.BayesClassifier();
+
+// For all the categories specified in training.json,
+// we train the classifier on these sample text.
+// e.g. classifier.addDocument('lets get @chris onboarded.', 'member_start');
+// for (var key in training) {
+//   classifier.addDocument(training[key], key);
+// }
+for (var key in training) {
+  training[key].forEach(function(text) {
+    classifier.addDocument(text, key);
+  });
+}
+classifier.train();
+
 // Set-up handler for bot's direct messaging response
 controller.on('direct_message', function(bot, message) {
-  bot.reply(message, 'Hey there 😜, it\'s just you and me.');
+  var category = classifier.classify(message.text);
+  bot.reply(message, category);
 });
 
 // Set-up handler for bot's direct mention response.
 // Is available in channels that bot is in.
 controller.on('direct_mention', function(bot, message) {
-  bot.reply(message, 'Hey there 😜');
+  var category = classifier.classify(message.text);
+  bot.reply(message, category);
 });
 
 // Export for testing
