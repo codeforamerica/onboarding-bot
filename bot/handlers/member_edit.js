df8c8d710883df5a331ec42c8f90cb2369b9b23d
Edmund Korley <korley.em@gmail.com>
2016-06-17 14:17:02 -0700

    Clean up member_* handlers draft

--- bot/handlers/member_edit.js
+++ bot/handlers/member_edit.js
@@ -4,5 +4,5 @@ var chrono = require('chrono-node');
 module.exports = function(text) {
   var date = chrono.parseDate(text);
   var person = text.match(/\<\@[a-z0-9]+\>/gim);
-  return 'Hey, ' + person + ' will be onboarded ' + date + '! 🚢💻🇺🇸';
+  return 'Ok, I\'ll update ' + person + '\'s schedule. They started ' + date + '! 🚢💻🇺🇸';
 }
--- bot/response.js
+++ bot/response.js
@@ -1,5 +1,7 @@
 var natural = require('natural');
 var member_start = require('./handlers/member_start');
+var member_info = require('./handlers/member_info');
+var member_edit = require('./handlers/member_edit');
 
 // Text to train Bayesian classifier on
 var training = require('./training.json');
@@ -21,6 +23,12 @@ module.exports = function(text, cb) {
   switch (category) {
     case 'member_start':
       cb(member_start(text));
+      break;
+    case 'member_info':
+      cb(member_info(text));
+      break;
+    case 'member_edit':
+      cb(member_edit(text));
       break;
     default:
       cb('I don\'t understand that 😕');
