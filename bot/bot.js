3ae8651d6d1949416dd4cc2a62a0633eb01caa65
Edmund Korley <korley.em@gmail.com>
2016-06-23 12:10:45 -0700

    DB query hooks into bot logic

--- bot/bot.js
+++ bot/bot.js
@@ -1,28 +1,41 @@
 var Botkit = require('botkit');
-var response = require('./response');
+var botResponse = require('./response');
+var pg = require('pg').native;
+var config = require('../config.json');
+var queryDB = require('./query.js');
+var connectionString = process.env.DEV ? config.settings.dev.db : config.settings.prod.db;
 
 // Our configuration and initialization for botkit
-var config = require('../config.json');
 var setup = process.env.DEV ? config.settings.dev : config.settings.prod;
 var controller = Botkit.slackbot(setup);
 controller.spawn(config.authentication).startRTM();
 
-// Set-up handler for bot's direct messaging response
-controller.on('direct_message', function(bot, message) {
-  console.log('RECEIVED', message);
-  response(message.text, function (text) {
-    bot.reply(message, text);
-  });
-});
+// Connect to our Postges database
+pg.connect(connectionString, function(err, client) {
+    if (err) console.warn('ERROR 🚫:\n', err);
 
-// Set-up handler for bot's direct mention response.
-// Is available in channels that bot is in.
-controller.on('direct_mention', function(bot, message) {
-  console.log('RECEIVED', message);
-  response(message.text, function (text) {
-    bot.reply(message, text);
-  });
-});
+    // Dummy request for latest_message
+    queryDB(client, function(result) {
+      console.log('SUCCESS ✅:\n', result);
+    });
 
-// Export for testing
-module.exports = controller;
+    // Set-up handler for bot's direct messaging response
+    controller.on('direct_message', function(bot, message) {
+      console.log('RECEIVED', message);
+      botResponse(message.text, client, function (text) {
+        bot.reply(message, text);
+      });
+    });
+
+    // Set-up handler for bot's direct mention response.
+    // Is available in channels that bot is in.
+    controller.on('direct_mention', function(bot, message) {
+      console.log('RECEIVED', message);
+      botResponse(message.text, function (text) {
+        bot.reply(message, text);
+      });
+    });
+
+    // Export for testing
+    module.exports = controller;
+});
