897867734fc488f9caeb85ad4a52fb4d7e5c46bc
Edmund Korley <korley.em@gmail.com>
2016-06-24 13:11:56 -0700

    Grunt and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: './public/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
