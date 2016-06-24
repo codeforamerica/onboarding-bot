cb112f37306a4aef426c5b54e6a4ab0acf069ce5
Edmund Korley <korley.em@gmail.com>
2016-06-24 13:13:00 -0700

    React scaffold

---
+++ app/components/Base.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Queries from './Queries.jsx';
+
+export default class Base extends Component {
+  render() {
+    return <div>
+        <Queries />
+    </div>
+  }
+}
---
+++ app/components/Queries.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class Queries extends Component {
+  render() {
+    return <div>
+    </div>
+  }
+}
