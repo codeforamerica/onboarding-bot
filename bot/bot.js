ab9a701a6b5a58c8ad95c4ee752c74ecb4796ca6
Edmund Korley <korley.em@gmail.com>
2016-08-02 16:20:17 -0700

    var => let

--- bot/bot.js
+++ bot/bot.js
@@ -8,30 +8,30 @@ const _ = require('lodash');
 const Botkit = require('botkit');
 const pg = require('pg').native;
 
-var botResponse = require('./response');
-var config;
+let botResponse = require('./response');
+let config;
 try {
   config = require('../config.json');
 } catch (e) {
   console.warn('Could not find local config.json, assumming production environment.\n');
   config = {'settings':{'prod':{'debug':'false','db':process.env.DATABASE_URL},'dev':{'debug':'false','db':process.env.DATABASE_URL}},'authentication_prod':{'token':process.env.SLACK},'authentication_dev':{'token':process.env.SLACK}};
 }
-var connectionString = process.env.DEV ? config.settings.dev.db : config.settings.prod.db;
+let connectionString = process.env.DEV ? config.settings.dev.db : config.settings.prod.db;
 
 // Our configuration and initialization for botkit
-var setup = process.env.DEV ? config.settings.dev : config.settings.prod;
-var controller = Botkit.slackbot(setup);
-var authentication = process.env.DEV ? config.authentication_dev : config.authentication_prod;
+let setup = process.env.DEV ? config.settings.dev : config.settings.prod;
+let controller = Botkit.slackbot(setup);
+let authentication = process.env.DEV ? config.authentication_dev : config.authentication_prod;
 controller.spawn(authentication).startRTM();
 
 // Setup our express server, for serving front-end interface
 const express = require('express');
 const bodyParser = require('body-parser');
-var server = express();
+let server = express();
 server.use(bodyParser.json()); // for parsing application/json
 server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-var port = process.env.PORT || 8000;
-var dir = path.join(__dirname, '/../public');
+let port = process.env.PORT || 8000;
+let dir = path.join(__dirname, '/../public');
 
 // Set up our front-end API
 server.use(express.static(dir));
@@ -41,7 +41,7 @@ pg.connect(connectionString, function(err, client) {
     if (err) console.warn('ERROR 🚫:\n', err);
 
     // Modes of interaction
-    var modes = ['direct_mention', 'direct_message', 'channel_joined', 'channel_leave'];
+    let modes = ['direct_mention', 'direct_message', 'channel_joined', 'channel_leave'];
 
     // Set-up bot's handlers
     controller.on(modes, function(bot, message) {
@@ -71,15 +71,15 @@ pg.connect(connectionString, function(err, client) {
 function fieldsfor(field) {
   switch (field) {
     case 'members':
-      var words = ['members_id', 'members_name', 'members_descript', 'last_message_id'];
+      let words = ['members_id', 'members_name', 'members_descript', 'last_message_id'];
       return words;
       break;
     case 'messages':
-      var words = ['messages_id', 'messages_text', 'receivers_id', 'receivers_name', 'time_to_post', 'senders_id', 'senders_name'];
+      let words = ['messages_id', 'messages_text', 'receivers_id', 'receivers_name', 'time_to_post', 'senders_id', 'senders_name'];
       return words;
       break;
     case 'resources':
-      var words = ['resources_id', 'resources_title', 'resources_keywords', 'resources_link'];
+      let words = ['resources_id', 'resources_title', 'resources_keywords', 'resources_link'];
       return words;
       break;
     default:
