e77369a3bdcc5efc0bdac1fc446543331f7f5a94
Edmund Korley <korley.em@gmail.com>
2016-08-04 14:58:49 -0700

    WIP convo for add_training

--- bot/handlers/add_training.js
+++ bot/handlers/add_training.js
@@ -15,63 +15,79 @@ mutation {
 `;
 
 module.exports = function(text, client, bot, message) {
-  return graphql(schema, query).then((result) => {
-
     bot.startConversation(message, function(err, convo) {
 
         if (err) console.log(`***** ERROR ${err}`);
 
         convo.ask(
-          // 'Do you want to teach me a new phrase?',
-          // The 'message' the bot is going to ask
-          {
-            attachments: [
-                {
-                    title: 'Great, do you want to teach me a new phrase?',
-                    callback_id: 'abc123',
-                    attachment_type: 'default',
-                    actions: [
-                        {
-                            "name":"yes",
-                            "text": "Yes",
-                            "value": "yes",
-                            "type": "button",
-                        },
-                        {
-                            "name":"no",
-                            "text": "No",
-                            "value": "no",
-                            "type": "button",
-                        }
-                    ]
-                }
-            ]
-        },
-        // The specified callbacks that will run given users response
-        [
-            {
-                pattern: "yes",
-                callback(reply, convo) {
-                    console.log('***** CB RAN');
-                    convo.say(`What phrase do you want me to learn? \n ${reply}`);
-                    convo.say(`Ok, I\'ved updated my database. Here is the resulting unique id of the transaction ${32133322}! 💻🇺🇸`)
-                    convo.next();
-                }
-            },
-            {
-                pattern: "no",
-                callback(reply, convo) {
-                    console.log('***** CB RAN', JSON.stringify(bot.utterances, null, '\t'));
+          'Do you want to teach me a new phrase?',
+          [
+              {
+                  pattern: "yes",
+                  callback(reply, convo) {
+                      convo.ask(
+                        `
+                          What category do you want to put this new phrase you are about to teach me?\n
+                          ${process.env.DEV ? JSON.stringify(reply, null, '\t') : ""}
+                        `,
+                        [
+                          {
+                            pattern: 'nevermind',
+                            callback(reply, convo) {
+                              convo.say('Ok then ... 🙂');
+                            }
+                          },
+                          {
+                              default: true,
+                              callback(reply, convo) {
+                                convo.ask(
+                                  `
+                                    Ok, type out the training text associated with the category\n
+                                    ${process.env.DEV ? JSON.stringify(reply, null, '\t') : ""}
+                                  `,
+                                  [
+                                    {
+                                      default: true,
+                                      callback(reply, convo) {
+                                        convo.say(`I've will try and add '${reply}' to my database.`);
+                                        graphql(schema, query)
+                                          .then((result) => {
+                                            convo.say(
+                                              `I did it! Here is the result:\n
+                                              ${process.env.DEV ? JSON.stringify(reply, null, '\t') : ""}
+                                              `
+                                            );
+                                            convo.next();
+                                          })
+                                          .catch((error) => {
+                                            convo.say(
+                                              `
+                                                I couldn't do that heres why:\n
+                                                ${JSON.stringify(error, null, '\t')}
+                                              `
+                                            );
+                                            convo.next();
+                                          });
+                                      }
+                                    }
+                                  ]); // Training text response
+                              }
+                          }
+                        ]); // Category response
+                  }
+              },
+              {
+                  pattern: "no",
+                  callback(reply, convo) {
                     convo.say('Ok 🙂');
-                    convo.next();
-                }
-            },
-            {
-                default: true,
-                callback(reply, convo) {}
-            }
-        ]);
-    });
-
+                  }
+              },
+              {
+                  default: true,
+                  callback(reply, convo) {
+                    convo.say('Didn\'t quite get that 😕');
+                  }
+              }
+          ]); // Confirmation response
   });
 }
